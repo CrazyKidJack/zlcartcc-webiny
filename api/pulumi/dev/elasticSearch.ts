d410722899a0584e5e6d4870a65f28053baa47c4
Jackson Pfeffer <crazykidjack@gmail.com>
2021-05-29 04:59:01 -0500

    changing back to t3.small.elasticsearch

--- api/pulumi/dev/elasticSearch.ts
+++ api/pulumi/dev/elasticSearch.ts
@@ -12,7 +12,7 @@ class ElasticSearch {
         this.domain = new aws.elasticsearch.Domain(domainName, {
             elasticsearchVersion: "7.7",
             clusterConfig: {
-                instanceType: "t2.micro.elasticsearch"
+                instanceType: "t3.small.elasticsearch"
             },
             ebsOptions: {
                 ebsEnabled: true,
