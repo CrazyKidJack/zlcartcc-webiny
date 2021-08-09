be83f13e23b7157d17f3dd6f1b3fccf56ed2fa8d
CrazyKidJack <43480837+CrazyKidJack@users.noreply.github.com>
2021-08-08 22:24:53 -0500

    Clearing out the repo

--- api/code/dynamoToElastic/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createHandler } from "@webiny/handler-aws";
-import elasticSearch from "@webiny/api-plugin-elastic-search-client";
-import dynamoDBToElastic from "@webiny/api-dynamodb-to-elasticsearch/handler";
-
-export const handler = createHandler(
-    elasticSearch({ endpoint: `https://${process.env.ELASTIC_SEARCH_ENDPOINT}` }),
-    dynamoDBToElastic()
-);
